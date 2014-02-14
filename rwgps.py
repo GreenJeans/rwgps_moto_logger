1622fedcaaf1fed214620548b2e385ea562a2fab
Tyler Morita <tmorita@ebay.com>
2014-02-14 14:58:44 -0800

    Init

---
+++ rwgps.py
@@ -0,0 +1,39 @@
+__author__ = 'tylermorita'
+
+
+import requests
+from test_payloads import payload
+
+
+session = requests.session()
+
+
+def login(email, password):
+    '''get and set a session token'''
+    url = 'https://ridewithgps.com/users/current.json'
+    params = {'android_version': 19,
+             'device_manufacturer': 'motorola',
+             'device_model': 'XT1060',
+             'documentation_version': 1,
+             'email': email,
+             'app_version': '1.0.33',
+             'password': password,
+             'version': '2',
+             'apikey': 'mzeanzgk'
+    }
+
+    resp = session.get(url, params=params)
+    session.params = {'auth_token': resp.json()['user']['auth_token'],
+                      'api_key': 'mzeanzgk'}
+    return
+
+
+def submit_trip():
+    url = 'https://ridewithgps.com/trips'
+    resp = session.post(url, data=payload)
+    return resp
+
+if __name__ == '__main__':
+    login('tyler.morita@gmail.com', 'ch0k3r53773R')
+    resp = submit_trip()
+    print resp.headers, resp.text
