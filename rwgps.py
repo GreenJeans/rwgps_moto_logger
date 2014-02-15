548b5113d45a6616de53cc5de4c84c17247c9fb1
Tyler Morita <tmorita@ebay.com>
2014-02-14 18:55:14 -0800

    Fixed stuff

--- rwgps.py
+++ rwgps.py
@@ -39,13 +39,13 @@ def make_payload(csv_file, name):
     track_points = []
     for row in csv_file:
         track_points.append({"x": float(row['GPS Longitude']),
-                      "y": float(row['GPS Latitude']),
-                      "e": 10.0,
-                      "t": int(row['time']) / 1000,
-                      "h": float(row['RPM']),
-                      "s": int(round(float(row['GPS Speed']) * .447)),
-                      "c": float(row['Air Fuel Ratio (alt)']),
-                      "p": float(row['Boost'])})
+                             "y": float(row['GPS Latitude']),
+                             "e": 10.0,
+                             "t": int(row['time']) / 1000,
+                             "h": float(row['RPM']),
+                             "s": round((float(row['GPS Speed']) * .447), 2),
+                             "c": float(row['Air Fuel Ratio (alt)']),
+                             "p": float(row['Boost'])})
     payload = {'trip': {'bad_elevations': True,
                         'visibility': 0,
                         'name': name,
@@ -58,11 +58,12 @@ def make_payload(csv_file, name):
 if __name__ == '__main__':
     session = requests.session()
     session.params = {'auth_token': config.auth_token,
-                      'api_key': 'as90b8eu'}
+                      'api_key': config.api_key}
     session.headers = {'content-type': 'application/json'}
 
     # login(config.email, config.password)
     payload = make_payload('1392425135096.csv', 'DriveHome')
     resp = submit_trip(json.dumps(payload))
-    print 'Request:\nHeaders:\n{}\n\nParams:\n{}\n\nBody:\n{}\n\n'.format(session.headers, session.params, json.dumps(payload, indent=4))
+    # print 'Request:\nHeaders:\n{}\n\nParams:\n{}\n\nBody:\n{}\n\n'.format(session.headers, session.params,
+    #                                                                       json.dumps(payload, indent=4))
     print 'Response:\nHeaders:\n{}\n\nBody:\n{}'.format(resp.headers, resp.text)
